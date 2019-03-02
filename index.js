ea5d9552204619fa9e94de836e98d3d3e9570f13
Huong <admin@Apples-MacBook-Pro.net.fpt>
2019-03-02 14:51:41 +0700

    form add

--- index.js
+++ index.js
@@ -7,5 +7,8 @@ const { Singer, arrSinger, domain, domainImg } = require('./models/Singer')
 app.get('/',(req,res)=>{
     res.render('list',{arrSinger,domainImg,domain});
 });
+app.get('/add',(req,res)=>{
+    res.render('add');
+});
 
 app.listen(3000);

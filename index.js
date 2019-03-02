51b11cdc161cbf6cf91d3557b444b9ddcc5e1392
Huong <admin@Apples-MacBook-Pro.net.fpt>
2019-03-02 15:20:31 +0700

    body-parser

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 app.set('view engine','ejs');
+
+app.use(bodyParser.urlencoded({
+    extended:false // string || array
+}))
 
 const { Singer, arrSinger, domain, domainImg } = require('./models/Singer')
 
@@ -8,7 +13,14 @@ app.get('/',(req,res)=>{
     res.render('list',{arrSinger,domainImg,domain});
 });
 app.get('/add',(req,res)=>{
+    // req.params
     res.render('add');
+});
+app.post('/add-singer',(req,res)=>{
+    const {txtName, txtAvatar, txtLink} = req.body
+    res.send({
+        txtName, txtAvatar, txtLink
+    })
 });
 
 app.listen(3000);

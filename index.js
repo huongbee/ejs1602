6e94d1236d9697b2bfbee9ad830eea54f3ad7d79
Huong <admin@Apples-MacBook-Pro.net.fpt>
2019-03-02 14:40:41 +0700

    list

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-
 const app = express();
 app.set('view engine','ejs');
 
+const { Singer, arrSinger, domain, domainImg } = require('./models/Singer')
+
 app.get('/',(req,res)=>{
-    res.render('list');
+    res.render('list',{arrSinger,domainImg,domain});
 });
 
 app.listen(3000);
